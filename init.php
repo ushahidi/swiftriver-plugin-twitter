1d2986720dd134048a3ca28aa59bb2dc40ff68a9
Emmanuel Kala <emkala@gmail.com>
2013-06-11 16:41:29 +0300

    Do register Twitter crawler

--- init.php
+++ init.php
@@ -17,9 +17,6 @@ class Twitter_Init {
 
 	public function __construct()
 	{
-	    // Register as a crawler
-	    Swiftriver_Crawlers::register('twitter', array(new Swiftriver_Crawler_Twitter(), 'crawl'));
-	    	    
 		// Validate Channel Filter Settings Input
 		Swiftriver_Event::add('swiftriver.channel.validate', array($this, 'validate'));
 		
